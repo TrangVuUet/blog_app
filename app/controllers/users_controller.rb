e621ead363347ff031104e21a76e29a5e6070e15
TrangVu <trangvuuet@gmail.com>
2016-07-17 09:53:38 +0700

    doing login-logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Write your first blog!"
       redirect_to @user
     else
